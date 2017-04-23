9ac76127649ef8be1df014d3f9174f2002f206e4
Jordan Walke <jordojw@gmail.com>
2017-04-23 00:57:17 -0700

    Instructions on how to update hosts without node

--- scripts/performBuildEject.sh
+++ scripts/performBuildEject.sh
@@ -2,7 +2,12 @@
 
 set -e
 
-# Peform build eject
+# Peform build eject.
+# Warms up *just* the artifacts that require having a modern node installed.
 cd ./actualInstall/
+# Generates all the make files etc.
 (../node_modules/.bin/esy build-eject || (ls ./node_modules/.cache/**/* && echo "FAILED TO BUILD-EJECT"))
+# Ensure the shell script is generated which constructs environments to execute
+# commands within.
+(../node_modules/.bin/esy dump-env || (ls ./node_modules/.cache/**/* && echo "FAILED TO DUMP-ENV"))
 cd ..
