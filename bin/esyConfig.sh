446c35ae6cc73eb356d2e0fc81b60370866a08b0
Jordan Walke <jordojw@gmail.com>
2017-12-15 23:42:07 -0800

    Preparing release 3.0.4-bin-linux

---
+++ bin/esyConfig.sh
@@ -0,0 +1,8 @@
+# Esy version
+export ESY__VERSION="0.0.61"
+# Esy store format version
+export ESY__STORE_VERSION="3"
+# Esy metadata format version
+export ESY__METADATA_VERSION="4"
+# Store path padding required for relocatable artifacts
+export ESY__STORE_PADDING_LENGTH="86"
